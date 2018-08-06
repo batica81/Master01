b7a5972b072cddcef5572e64e159a0cda9f1a69d
batica81 <batica@gmail.com>
2018-08-06 22:10:05 +0200

    Radi klijent cert! (tls 1.2 kitkat bug prisutan)

--- app/src/main/java/com/example/voja/master01/MainActivity.java
+++ app/src/main/java/com/example/voja/master01/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.voja.master01;
 
+import android.os.Environment;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,20 +8,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.cedarsoftware.util.io.JsonWriter;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
@@ -32,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText passwordEditText;
     private TextView myTextView;
     private Certificate[] sviSertifikati;
+
+//    private String certificateFile = Environment.getExternalStorageDirectory().getPath() + "batica+1434@gmail.com.p12";
+    private String certificateFile = "/sdcard/Download/batica+1434@gmail.com.p12";
+    private String clientCertPassword = "204700";
 
     private String username =  "";
     private String password = "";
@@ -60,12 +75,25 @@ public class MainActivity extends AppCompatActivity {
                     makeSearch();
                 } catch (IOException e) {
                     e.printStackTrace();
+                    displayExceptionMessage(e.getMessage());
                 } catch (JSONException e) {
                     e.printStackTrace();
+                    displayExceptionMessage(e.getMessage());
                 } catch (NoSuchAlgorithmException e) {
                     e.printStackTrace();
+                    displayExceptionMessage(e.getMessage());
                 } catch (KeyManagementException e) {
                     e.printStackTrace();
+                    displayExceptionMessage(e.getMessage());
+                } catch (CertificateException e) {
+                    e.printStackTrace();
+                    displayExceptionMessage(e.getMessage());
+                } catch (UnrecoverableKeyException e) {
+                    e.printStackTrace();
+                    displayExceptionMessage(e.getMessage());
+                } catch (KeyStoreException e) {
+                    e.printStackTrace();
+                    displayExceptionMessage(e.getMessage());
                 }
             }
         });
@@ -76,8 +104,10 @@ public class MainActivity extends AppCompatActivity {
                     showCert();
                 } catch (IOException e) {
                     e.printStackTrace();
+                    displayExceptionMessage(e.getMessage());
                 } catch (JSONException e) {
                     e.printStackTrace();
+                    displayExceptionMessage(e.getMessage());
                 }
             }
         });
@@ -88,18 +118,32 @@ public class MainActivity extends AppCompatActivity {
                     killIt();
                 } catch (IOException e) {
                     e.printStackTrace();
+                    displayExceptionMessage(e.getMessage());
                 } catch (JSONException e) {
                     e.printStackTrace();
+                    displayExceptionMessage(e.getMessage());
                 }
             }
         });
     }
 
-    private void makeSearch() throws IOException, JSONException, NoSuchAlgorithmException, KeyManagementException {
 
-        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
-        TrustManager[] trustManagers = new TrustManager[] { new TrustManagerManipulator() };        sslContext.init(null, trustManagers, new SecureRandom());
-        SSLSocketFactory noSSLv3Factory = new TLSSocketFactory(sslContext.getSocketFactory());
+
+    public void displayExceptionMessage(String msg) {
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        }
+
+    private void makeSearch() throws IOException, JSONException, NoSuchAlgorithmException, KeyManagementException, KeyStoreException, CertificateException, UnrecoverableKeyException {
+
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        InputStream fis = new FileInputStream(certificateFile);
+        keyStore.load(fis, clientCertPassword.toCharArray());
+
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("X509");
+        kmf.init(keyStore, clientCertPassword.toCharArray());
+        KeyManager[] keyManagers = kmf.getKeyManagers();
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagers, null, null);
 
 
         username = String.valueOf(usernameEditText.getText());
@@ -108,12 +152,13 @@ public class MainActivity extends AppCompatActivity {
         URL url = new URL(myUrl);
         HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
 
-        urlConnection.setSSLSocketFactory(noSSLv3Factory);
+        urlConnection.setSSLSocketFactory(sslContext.getSocketFactory());
 
         urlConnection.setRequestMethod("POST");
         urlConnection.setDoInput(true);
         urlConnection.setDoOutput(true);
         urlConnection.setRequestProperty("Accept", "application/json");
+        urlConnection.setRequestProperty("Content-Type", "application/json");
         urlConnection.setRequestProperty("Content-Type", "application/json");
         JSONObject myJsonData = new JSONObject();
         myJsonData.put("Username", username);
@@ -124,8 +169,13 @@ public class MainActivity extends AppCompatActivity {
         OutputStream os = urlConnection.getOutputStream();
         os.write(outputBytes);
 
-        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-        String jsonString2 = IOUtils.toString(in);
+        InputStream inputStream = urlConnection.getErrorStream();
+        if (inputStream == null) {
+            inputStream = urlConnection.getInputStream();
+        }
+
+//        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+        String jsonString2 = IOUtils.toString(inputStream);
         String jsonString3 = JsonWriter.formatJson(jsonString2);
         myTextView.setText(jsonString3);
 
